71a311cd843d3b8c1f0d5de60155652fc8a6249f
Midori <chadifferus@gmail.com>
2018-08-11 13:36:56 -0300

    Created Powerup abstract class and lazer class. Updated player controller script to have SetPowerUp

---
+++ LudumDare42/Assets/Scripts/Powerups/LaserPowerup.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserPowerup : PowerUp {
+
+	public override void GivePowerUp() {
+		controller.SetPowerUp(1);
+	}
+}
