1c53556d0da7a8fbb07053abdab1ab022b139ee2
Logan Marks <logicon211@gmail.com>
2018-08-11 14:22:02 -0300

    melee robot tiny script update

--- LudumDare42/Assets/Scripts/Robots/Melee/MeleeRobot.cs
+++ LudumDare42/Assets/Scripts/Robots/Melee/MeleeRobot.cs
@@ -81,8 +81,10 @@ public class MeleeRobot : MonoBehaviour, IDamageable<float> {
 	}
 
 	public void finishPunch() {
-		//TODO: Check player distance and if they're still close, finish the punch, make the noise and do damage
-		Instantiate(hitEffect, new Vector3(player.transform.position.x, player.transform.position.y, player.transform.position.z - 0.1f), Quaternion.identity);
+		if(Vector2.Distance(player.transform.position, transform.position) <= attackRange) {
+			//TODO: Do damage to player
+			Instantiate(hitEffect, new Vector3(player.transform.position.x, player.transform.position.y, player.transform.position.z - 0.1f), Quaternion.identity);
+		}
 	}
 
 	// Damages enemy and handles death shit
