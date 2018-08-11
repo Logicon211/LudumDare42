453e1ab07475f04aa93ab36badf70b57d7d19988
Midori <chadifferus@gmail.com>
2018-08-10 23:46:51 -0300

    Created Scripts for Ranged Robot and personal test scene

---
+++ LudumDare42/Assets/Scripts/RangeRobotController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RangeRobotController : MonoBehaviour {
+
+	[Range(0, .3f)] [SerializeField] private float movementSmoothing = .05f;
+	[SerializeField] private float shootCooldown = 5f;
+	public GameObject projectile;
+
+	private Rigidbody2D enemyBody;
+	private Vector3 velocity = Vector3.zero;
+	private float currentCooldown;
+	private bool hasShot = false;
+
+	private void Awake() {
+		enemyBody = GetComponent<Rigidbody2D>();
+		currentCooldown = shootCooldown;
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Handling shooting cooldown
+		if (hasShot) {
+			currentCooldown -= Time.deltaTime;
+			if (currentCooldown <= 0f) {
+				currentCooldown = shootCooldown;
+				hasShot = false;
+			}
+		}
+	}
+
+	// Moves the enemy towards the x and y given
+	public void Move(float tarX, float tarY) {
+		
+		//Create a new velocity then smooth is out when we apply it
+		Vector3 targetVelocity = new Vector2(tarX * 10f, tarY * 10f);
+		enemyBody.velocity = Vector3.SmoothDamp(enemyBody.velocity, targetVelocity, ref velocity, movementSmoothing);		
+	}
+
+	// Rotates the enemy to face the player
+	public void Rotate() {
+
+	}
+
+	// Shoots a bullet towards the given x and y
+	public void Shoot(float tarX, float tarY) {
+		if (!hasShot) {
+			GameObject bullet = Instantiate(projectile, transform.position, Quaternion.identity) as GameObject;
+            bullet.GetComponent<Rigidbody2D>().velocity = new Vector2(tarX * 10f, tarY * 10f);
+			hasShot = true;
+		}
+	}
+}
---
+++ LudumDare42/Assets/Scripts/RangeRobotMovement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RangeRobotMovement : MonoBehaviour {
+
+	public RangeRobotController controller;
+	public float maxDistance = 20f;
+	public float shootDistance = 40f;
+	public float speed = 10f;
+	public float shootSpeed = 20f;
+
+	private GameObject player;
+	private Transform playerTransform;
+	private bool move = false;
+	private bool shoot = false;
+	private Vector3 moveDir = Vector3.zero;
+	private Vector3 shootDir = Vector3.zero;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float dist = Vector3.Distance(player.transform.position, transform.position);
+		if (dist > maxDistance) {
+			move = true;
+		}
+		if (dist < shootDistance) {
+			shoot = true;
+		}
+
+	}
+
+	void FixedUpdate() {
+		if (move) {
+			moveDir = (player.transform.position - transform.position).normalized;	
+		}
+		if (shoot) {
+			shootDir = (player.transform.position - transform.position).normalized;	
+		}
+		controller.Move(moveDir.x * speed * Time.fixedDeltaTime, moveDir.y * speed * Time.fixedDeltaTime);
+		controller.Shoot(shootDir.x * shootSpeed * Time.fixedDeltaTime, shootDir.y * shootSpeed * Time.fixedDeltaTime);
+		moveDir = Vector3.zero;
+		shootDir = Vector3.zero;
+		move = false;
+		shoot = false;
+	}
+}
