0c408b447e0c4628ffd9509ee42eeac8527f3958
BroderickBeaman <broderick.beaman@unb.ca>
2018-08-11 14:56:20 -0300

    Garbage Controller WIP

--- LudumDare42/Assets/Scripts/Garbage/GarbageController.cs
+++ LudumDare42/Assets/Scripts/Garbage/GarbageController.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class GarbageController : MonoBehaviour {
 
-	public float garbageSpawnYOffset = 5f;
+	public float spawnYOffset = 5f;
+	public float dropSpeed = 1f;
+
+	public Material shadowMaterial;
 
 	private SpriteRenderer casterRenderer;
 	private SpriteRenderer shadowRenderer;
 
 	private GameObject shadowObject;
-	private bool isGarbageDropping;
+	private bool isDropping;
 
 	// Use this for initialization
 	void Start () {
@@ -20,27 +23,41 @@ public class GarbageController : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		if (isGarbageDropping) {
-			DropGarbage ();
-		}
+		
+	}
+
+	void FixedUpdate () {
+//		if (isDropping) {
+//			DropGarbage ();
+//		}
 	}
 
 	private void StartGarbageDrop () {
 		Vector2 topOfCameraInViewportCoordinates = new Vector2 (1f, 1f);
 		Vector2 topOfCameraInWorldCoordinates = Camera.main.ViewportToWorldPoint (topOfCameraInViewportCoordinates);
 		float topOfCameraY = topOfCameraInWorldCoordinates.y;
-		transform.position = new Vector2 (transform.position.x, topOfCameraY + garbageSpawnYOffset); 
-		isGarbageDropping = true;
+		transform.position = new Vector2 (transform.position.x, topOfCameraY + spawnYOffset); 
+		isDropping = true;
 	}
 
 	private void InitShadow () {
 		shadowObject = new GameObject ();
+		shadowObject.transform.position = transform.position;
 		casterRenderer = GetComponent<SpriteRenderer> ();
 		shadowRenderer = shadowObject.AddComponent<SpriteRenderer> ();
+		shadowRenderer.sprite = casterRenderer.sprite;
+		shadowRenderer.material = shadowMaterial;
+		shadowRenderer.sortingLayerName = casterRenderer.sortingLayerName;
+		shadowRenderer.sortingOrder = casterRenderer.sortingOrder - 10;
 	}
 
 	private void DropGarbage () {
-		
+		float yPos = Mathf.Max (shadowObject.transform.position.y, transform.position.y - (dropSpeed * Time.fixedDeltaTime));
+		transform.position = new Vector2 (0f, yPos);
+		if (yPos <= shadowObject.transform.position.y) {
+			Object.Destroy (shadowObject);
+			//TODO: init collider and rigid body
+		}
 	}
 
 
