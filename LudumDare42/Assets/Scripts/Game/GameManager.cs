4e5500b126c0478be3c4dcf10d35249e571ab8a0
Logan Marks <logicon211@gmail.com>
2018-08-12 16:11:13 -0300

    Fixed? scene manager

--- LudumDare42/Assets/Scripts/Game/GameManager.cs
+++ LudumDare42/Assets/Scripts/Game/GameManager.cs
@@ -34,15 +34,15 @@ public class GameManager : MonoBehaviour {
 
 	private void Awake() {
 		//Check if instance already exists
-		if (instance == null)
-			//if not, set instance to this
-			instance = this;
-		//If instance already exists and it's not this:
-		else if (instance != this)
-			//Then destroy this. This enforces our singleton pattern, meaning there can only ever be one instance of a GameManager.
-			Destroy(gameObject);    
-		//Sets this to not be destroyed when reloading scene
-		DontDestroyOnLoad(gameObject);
+		// if (instance == null)
+		// 	//if not, set instance to this
+		// 	instance = this;
+		// //If instance already exists and it's not this:
+		// else if (instance != this)
+		// 	//Then destroy this. This enforces our singleton pattern, meaning there can only ever be one instance of a GameManager.
+		// 	Destroy(gameObject);    
+		// //Sets this to not be destroyed when reloading scene
+		// DontDestroyOnLoad(gameObject);
 		//Get a component reference to the attached BoardManager script
 		//boardScript = GetComponent<BoardManager>();
 		//Call the InitGame function to initialize the first level 
